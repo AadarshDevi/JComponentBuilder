29aecf76440308e7d960be4911399f632eff272e
Aadarsh D <aadarsh123456char@gmail.com>
2026-07-27 13:20:44 -0700

    renamed class and added the input field management object

--- src/main/java/com/aadarshdevi/jcbuilder/testing/StyleStage.java
+++ src/main/java/com/aadarshdevi/jcbuilder/testing/StyleStage.java
@@ -5,15 +5,37 @@ import javafx.scene.control.Control;
 import javafx.scene.layout.BorderPane;
 import javafx.stage.Stage;
 
-public class CustomStage extends Stage {
-    public CustomStage(Control control) {
+public class StyleStage extends Stage {
+
+    private final Control control;
+
+    public StyleStage(Control control) {
+        this.control = control;
         super();
         setTitle("Control: " + control.getClass().getSimpleName());
         BorderPane controller = new BorderPane(control);
         BorderPane cage = new BorderPane(controller);
         cage.setPrefSize(480.0, 560.0);
+        InputFieldManagement ifm = new InputFieldManagement();
+        ifm.setPrefWidth(cage.getPrefWidth() - 10);
+        ifm.setNode(control);
+        cage.setBottom(ifm);
         Scene scene = new Scene(cage);
         setScene(scene);
-//        initStyle(StageStyle.UNDECORATED); // removed when scene can move window
     }
+
+    public void setControl() {
+
+    }
+
+
+    public void setButton() {
+        setControl();
+    }
+
+    public void setBackground() {
+
+    }
+
+
 }

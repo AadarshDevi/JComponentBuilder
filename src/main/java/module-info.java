eb9aef6b505bec61fa6ed5340910a00fd47053b6
Aadarsh D <aadarsh123456char@gmail.com>
2026-07-27 13:56:58 -0700

    added lombok

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,6 +1,7 @@
 module com.aadarshdevi.jcbuilder {
     requires javafx.controls;
     requires javafx.fxml;
+    requires static lombok;
 
     opens com.aadarshdevi.jcbuilder to javafx.fxml;
     exports com.aadarshdevi.jcbuilder;

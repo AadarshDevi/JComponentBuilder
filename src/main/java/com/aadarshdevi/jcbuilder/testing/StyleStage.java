8301aa62dbe34dbe58b20fc31b9bfe8372b8aca2
Aadarsh D <aadarsh123456char@gmail.com>
2026-07-27 13:57:17 -0700

    added lombok Getter & Setter annotations

--- src/main/java/com/aadarshdevi/jcbuilder/testing/StyleStage.java
+++ src/main/java/com/aadarshdevi/jcbuilder/testing/StyleStage.java
@@ -4,38 +4,31 @@ import javafx.scene.Scene;
 import javafx.scene.control.Control;
 import javafx.scene.layout.BorderPane;
 import javafx.stage.Stage;
+import lombok.Getter;
+import lombok.Setter;
 
 public class StyleStage extends Stage {
 
-    private final Control control;
+    @Setter
+    @Getter
+    private Control control;
 
     public StyleStage(Control control) {
         this.control = control;
         super();
-        setTitle("Control: " + control.getClass().getSimpleName());
-        BorderPane controller = new BorderPane(control);
+        setTitle("Control: " + this.control.getClass().getSimpleName());
+        BorderPane controller = new BorderPane(this.control);
+        setMinHeight(560.0);
+        setMinWidth(420.0);
         BorderPane cage = new BorderPane(controller);
-        cage.setPrefSize(480.0, 560.0);
+        cage.setPrefSize(420.0, 560.0);
+        cage.setMinWidth(400.0);
         InputFieldManagement ifm = new InputFieldManagement();
         ifm.setPrefWidth(cage.getPrefWidth() - 10);
-        ifm.setNode(control);
+        ifm.setNode(this.control);
         cage.setBottom(ifm);
         Scene scene = new Scene(cage);
         setScene(scene);
     }
-
-    public void setControl() {
-
-    }
-
-
-    public void setButton() {
-        setControl();
-    }
-
-    public void setBackground() {
-
-    }
-
 
 }

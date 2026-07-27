738ab96e75c64b7f6bf57375d8e82f6cefb50be9
Aadarsh D <aadarsh123456char@gmail.com>
2026-07-27 13:34:13 -0700

    imports

--- src/main/java/com/aadarshdevi/jcbuilder/testing/InputFieldManagement.java
+++ src/main/java/com/aadarshdevi/jcbuilder/testing/InputFieldManagement.java
@@ -10,7 +10,8 @@ import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.Spinner;
 import javafx.scene.control.TextField;
-import javafx.scene.layout.GridPane;
+import javafx.scene.layout.*;
+import javafx.scene.paint.Paint;
 
 public class InputFieldManagement extends GridPane {
 
